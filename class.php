325e9c1e385750d04b09f4e029246584c937b0ad
Harish Dhanraj Sugandhi <68777106+harishsganndhi123@users.noreply.github.com>
2024-04-15 19:36:25 +0530

    class and getter setter done

--- class.php
+++ class.php
@@ -7,17 +7,27 @@ class Task
 {
 
     public $description;
-
+    public $completed = false;
 
     public function __construct($description)
     {
         $this->description = $description;
     }
 
+    public function completed()
+    {
+        $this->completed = true;
+    }
+
 
 }
 $task = new Task("Learn OOP");
 echo var_dump($task->description);
+
+var_dump($task->completed);
+$task->completed();
+var_dump($task->completed);
+
 
 
 ?>
